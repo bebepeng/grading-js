14d93a06d35e48fc4b44401dcc7684dee3f62db7
Bebe Peng <bebepeng@gmail.com>
2014-06-18 09:44:25 -0600

    grade parser returns movments and trends of students grades

--- spec/grade_parser_spec.js
+++ spec/grade_parser_spec.js
@@ -1,8 +1,32 @@
-describe("GradeParser", function() {
+describe("GradeParser", function () {
 
-  it("returns an array of up/down/even for the given array", function() {
-    var result = GradeParser.getMovements([6,3,5,4,3,4,4,5]);
-    expect(result).toEqual(["down","up","down","down","up","even","up"]);
+  it("returns an array of up/down/even for the given array", function () {
+    var result = GradeParser.getMovements([6, 3, 5, 4, 3, 4, 4, 5]);
+    expect(result).toEqual(["down", "up", "down", "down", "up", "even", "up"]);
+  });
+
+  it("finds a downwards trend in a student's decreasing grades", function () {
+    var movements = GradeParser.getMovements([10, 9, 8, 7]);
+    var result = GradeParser.getTrend(movements);
+    expect(result).toEqual("in decline");
+  });
+
+  it("does not find a downwards trend in a student's increasing grades", function () {
+    var movements = GradeParser.getMovements([6, 7, 8, 9]);
+    var result = GradeParser.getTrend(movements);
+    expect(result).toEqual("not in decline");
+  });
+
+  it("does not find a downwards trend in a student's grades if there are not enough results", function () {
+    var movements = GradeParser.getMovements([10, 9, 8]);
+    var result = GradeParser.getTrend(movements);
+    expect(result).toEqual("not in decline");
+  });
+
+  it("does not find a downwards trend in a student's grades if there are not 3 downs in a row", function () {
+    var movements = GradeParser.getMovements([6, 3, 5, 4, 3, 4, 4, 5]);
+    var result = GradeParser.getTrend(movements);
+    expect(result).toEqual("not in decline");
   });
 
 });
--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,7 +1,37 @@
 window.GradeParser = {
 
   getMovements: function (grades) {
-    return [];
+    var movements = [];
+
+    for (i = 1; i < grades.length; i++) {
+      if (grades[i - 1] < grades[i]) {
+        movements[movements.length] = "up";
+      } else if (grades[i - 1] == grades[i]) {
+        movements[movements.length] = "even";
+      } else {
+        movements[movements.length] = "down";
+      }
+    }
+
+    return movements;
+  },
+
+  getTrend: function (movements) {
+    var decreasing_in_a_row = 0;
+
+    for (i = 0; i < movements.length; i++) {
+      if (movements[i] == "up") {
+        decreasing_in_a_row = 0;
+      } else if (movements[i] == "down") {
+        decreasing_in_a_row = decreasing_in_a_row + 1;
+      }
+    }
+
+    if (decreasing_in_a_row >= 3) {
+      return "in decline";
+    } else {
+      return "not in decline";
+    }
   }
 
 };
